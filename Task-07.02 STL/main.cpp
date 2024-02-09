733f4fc1e8ee1e786a9d379f8b853fd229327ad2
Margaret Alade <alade.margaret@yandex.ru>
2024-02-09 19:28:23 +0300

    Initial commit

--- Task-07.02 STL/main.cpp
+++ Task-07.02 STL/main.cpp
@@ -6,9 +6,22 @@
 //
 
 #include <iostream>
-
+#include <set>
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "[IN_SIZE]: ";
+    int size, num;
+    std::cin >> size;
+    std::set<int, std::greater <int>> set_n;
+    std::cout << "[IN_ELEMENTS]: ";
+    for (int i = 0; i < size; i++) {
+        std::cin >> num;
+        set_n.insert(num);
+    }
+    std::cout << "[OUT]:\n";
+    for (const auto& elem : set_n) {
+        std::cout << elem << "\n";
+    }
+    
+    
     return 0;
 }
